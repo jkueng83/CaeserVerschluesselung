803db6ddd670c61cdad494ee5bc384108f3226de
jkueng83 <johannes.kueng@gmx.at>
2020-02-21 20:35:46 +0100

    Update

--- src/at/jku/Main.java
+++ src/at/jku/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
 
-        String eingabeUnverschluesselt = "DASISTDERTEXTUNDNOCHETWAS";
+        String eingabeUnverschluesselt = "DASISTDERTEXTUNDNOCHETWASJJJ";
 
         System.out.println("Caeser + add from git hub");
 
