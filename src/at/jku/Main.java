a724a70b5a11cc26b530d4b09c8ff93184f00148
jkueng83 <johannes.kueng@gmx.at>
2020-02-21 20:32:28 +0100

    version updated

--- src/at/jku/Main.java
+++ src/at/jku/Main.java
@@ -3,8 +3,55 @@ package at.jku;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        String eingabeUnverschluesselt = "DASISTDERTEXT";
+
         System.out.println("Caeser + add from git hub");
-        
+
+        String verschluesselung = "";
+
+        int stringLaenge = eingabeUnverschluesselt.length();
+
+        int counter = 10;
+
+        for (int i = 0; i < stringLaenge; i++) {
+            char car = eingabeUnverschluesselt.charAt(i);
+            car += counter;
+            verschluesselung += car;
+
+            counter++;
+
+            if (counter > 20) {
+                counter = 7;
+            }
+
+            System.out.print(car);
+
+        }
+
+        System.out.println(verschluesselung);
+
+        stringLaenge = verschluesselung.length();
+
+        String entschluesselt ="";
+
+        counter = 10;
+        for (int i = 0 ; i < stringLaenge ; i++){
+            char car = verschluesselung.charAt(i);
+            car -= counter;
+            entschluesselt += car ;
+
+            counter++;
+
+            if (counter > 20) {
+                counter = 7;
+            }
+
+        }
+
+        System.out.println(entschluesselt);
+
+
     }
 }
